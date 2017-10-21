935927dbe7c2f5d63372464fdc15d766b07b559c
Alex Taber <alexftaber@gmail.com>
2017-10-20 21:06:42 -0400

    Fix broken moderation implementations

--- extensions/Moderation/Moderation.py
+++ extensions/Moderation/Moderation.py
@@ -12,14 +12,18 @@ class Moderation:
 
     @commands.has_permissions(kick_members=True)
     @commands.command()
-    async def kick(self, ctx, member):
+    async def kick(self, ctx, member, *, reason=""):
         """Kick the specified member"""
         try:
             member = ctx.message.mentions[0]
             if ctx.message.author.top_role < member.top_role:
                 await ctx.send("You cannot kick {}".format(member))
                 return
-            await member.kick()
+            try:
+                await member.send("You have been kicked from SSSv4stro! Reason: {}".format(reason if reason else "No reason provided"))
+            except discord.errors.Forbidden:
+                print("DMing user failed")
+            await member.kick(reason=reason)
             await ctx.send("{} has been kicked".format(member))
         except IndexError:
             await ctx.send("Please mention a member")
@@ -28,15 +32,23 @@ class Moderation:
 
     @commands.has_permissions(ban_members=True)
     @commands.command()
-    async def ban(self, ctx, member, *, reason):
+    async def ban(self, ctx, member, *, reason=""):
         """Ban the specified member"""
         try:
+            member = ctx.message.mentions[0]
             if ctx.message.author.top_role < member.top_role:
                 await ctx.send("You cannot ban {}".format(member))
                 return
+            try:
+                await member.send("You have been banned from SSSv4stro! Reason: {}".format(reason if reason else "No reason provided"))
+            except discord.errors.Forbidden:
+                print("DMing user failed.")
             await member.ban(reason=reason, delete_message_days=0)
             await ctx.send("{} has been banned".format(member))
         except IndexError:
             await ctx.send("Please mention a member")
         except discord.errors.Forbidden:
             await ctx.send("That member cannot be banned")
+
+def setup(bot):
+    bot.add_cog(Moderation(bot))
