bb4795e6f921bc76fdcbaa33bff0ea292fd7dc58
Konngitas <konngitas@gmail.com>
2018-03-03 23:18:54 +0000

    Added roles.db database for storing custom roles and added commands f… (#10)
    * Added roles.db database for storing custom roles and added commands for adding, removing, and changing colour of custom roles
    
    * Change code to use discord.Utils.get instead of iterating over role list. Also added missing await and removed unnecessary returns.
    
    * Added a comma because people were annoying me

--- extensions/Misc/Misc.py
+++ extensions/Misc/Misc.py
@@ -1,17 +1,20 @@
 import datetime
 import os
+import sqlite3
 
 import discord
 import yaml
 from discord.ext import commands
 
-from Utilities.get_helper import get_channel
+from Utilities.get_helper import get_channel, get_user
 
 
 class Misc:
 
     def __init__(self, bot):
         self.bot = bot
+        self.role_db = sqlite3.connect('Database/role.db')
+        self.role_db_cursor = self.role_db.cursor()
 
         path = os.path.dirname(os.path.abspath(__file__))
         try:
@@ -89,6 +92,70 @@ class Misc:
                 await ctx.send("There are {} members in {}".format(len(channel.members), channel.name))
         else:
             await ctx.send("{} currently has {} members".format(ctx.guild.name, ctx.guild.member_count))
+
+    @commands.has_permissions(manage_roles=True)
+    @commands.command()
+    async def customrole(self, ctx, member, rolename):
+        """Connects a role to a member so they can use color(), as so: .customrole @member rolename"""
+        member = get_user(ctx.message, member)
+        if member:
+            role = discord.utils.get(ctx.guild.roles, name=rolename)
+            if role:
+                self.role_db.cursor().execute("CREATE TABLE IF NOT EXISTS roles (userid TEXT PRIMARY KEY, roleid TEXT)")
+                exists = self.role_db_cursor.execute("SELECT * FROM roles WHERE userid={}".format(member.id)).fetchone()
+                if exists:
+                    return await ctx.send("Member already has custom role")
+                self.role_db.cursor().execute("INSERT INTO roles VALUES(?, ?)", (member.id, role.id))
+                self.role_db.commit()
+                await ctx.send("Added role {} to member {}".format(role.name, member.name))
+            else:
+                await ctx.send("Invalid roleid")
+        else:
+            await ctx.send("Invalid member")
+
+    @commands.has_permissions(manage_roles=True)
+    @commands.command()
+    async def removecustomrole(self, ctx, member):
+        """Removes custom roleid from member, if any exists"""
+        member = get_user(ctx.message, member)
+        if member:
+            table = self.role_db_cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name = 'roles'").fetchone()
+
+            if table:
+                role = self.role_db_cursor.execute("SELECT roleid FROM roles WHERE userid={}".format(member.id))
+
+                if role:
+                    self.role_db_cursor.execute("DELETE from roles WHERE userid={}".format(member.id))
+                    self.role_db.commit()
+                else:
+                    await ctx.send("Member has no custom roles")
+            else:
+                await ctx.send("No custom roles have been added yet")
+        else:
+            await ctx.send("Invalid member")
+
+    @commands.command(aliases=["colour"])
+    async def color(self, ctx, color):
+        """Changes color of custom role, if able. Color is in hex, in format 0x..."""
+        user = ctx.author.id
+        table = self.role_db_cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name = 'roles'").fetchone()
+
+        if table:
+            roleid = self.role_db_cursor.execute("SELECT roleid FROM roles WHERE userid={}".format(user)).fetchone()
+            if roleid:
+				role = discord.utils.get(ctx.guild.roles, id=roleid)
+				if role:
+					try:
+						color = discord.Colour(int(color, 16))
+						await discord.Client.edit_role(ctx.message.server, role, colour=color)
+					except ValueError:
+						await ctx.send("Invalid role colour")
+				else:
+					await ctx.send("Role not found")
+            else:
+                await ctx.send("You dont have a custom role!")
+        else:
+            await ctx.send("No custom roles have been added yet")
 
 
 def setup(bot):
